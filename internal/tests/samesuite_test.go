818c0131f2772c27473992b9b1b43eeb17ef6a48
Harry Bond <15960568+thelolagemann@users.noreply.github.com>
2023-02-11 00:53:34 +0000

    samesuite

---
+++ internal/tests/samesuite_test.go
@@ -0,0 +1,56 @@
+package tests
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+const (
+	samesuiteROMPath = "roms/same-suite"
+)
+
+func newSamesuiteTestCollectionFromDir(suite *TestSuite, dir string) *TestCollection {
+	// more or less the same as mooneye as it uses the same pass/fail mechanism
+	romDir := filepath.Join(samesuiteROMPath, dir)
+	tc := suite.NewTestCollection(dir)
+
+	// read the directory
+	files, err := os.ReadDir(romDir)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() || filepath.Ext(file.Name()) != ".gb" {
+			continue
+		}
+
+		tc.Add(&mooneyeTest{
+			romPath: filepath.Join(romDir, file.Name()),
+			name:    file.Name(),
+		})
+	}
+
+	return tc
+}
+
+func testSamesuite(t *testing.T, roms *TestTable) {
+	// create top level test suite
+	tS := roms.NewTestSuite("samesuite")
+
+	// apu
+	newSamesuiteTestCollectionFromDir(tS, "apu")
+
+	// dma
+	newSamesuiteTestCollectionFromDir(tS, "dma")
+
+	// interrupt
+	newSamesuiteTestCollectionFromDir(tS, "interrupt")
+
+	// ppu
+	newSamesuiteTestCollectionFromDir(tS, "ppu")
+
+	// sgb
+	newSamesuiteTestCollectionFromDir(tS, "sgb")
+}
