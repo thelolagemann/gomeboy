7dc4573e0ca079373569fd9fc9d2aa6ff2f0757e
Harry Bond <15960568+thelolagemann@users.noreply.github.com>
2023-02-27 06:54:36 +0000

    improved serial tick func

--- internal/serial/serial.go
+++ internal/serial/serial.go
@@ -5,24 +5,43 @@ import (
 	"github.com/thelolagemann/go-gameboy/internal/types"
 )
 
-var currentSerial uint8
-
 // Device is a device that can be attached to the Controller.
 type Device interface {
 	Receive(bool)
 	Send() bool
 }
 
+// Controller is the serial controller. It is responsible for sending and
+// receiving data to and from devices.
+// Before a transfer, data holds the next byte to be sent. AKA types.SB
+// During a transfer, it has a mix of the incoming data and the outgoing data.
+// each cycle, the leftmost bit of data is sent to the attached device, and
+// shifted out of data, and the incoming bit is shifted into data.
+//
+// example:
+//
+//	Before : data = o7 o6 o5 o4 o3 o2 o1 o0
+//	Cycle 1: data = o6 o5 o4 o3 o2 o1 o0 i0
+//	Cycle 2: data = o5 o4 o3 o2 o1 o0 i0 i1
+//	Cycle 3: data = o4 o3 o2 o1 o0 i0 i1 i2
+//	Cycle 4: data = o3 o2 o1 o0 i0 i1 i2 i3
+//	Cycle 5: data = o2 o1 o0 i0 i1 i2 i3 i4
+//	Cycle 6: data = o1 o0 i0 i1 i2 i3 i4 i5
+//	Cycle 7: data = o0 i0 i1 i2 i3 i4 i5 i6
+//	Cycle 8: data = i0 i1 i2 i3 i4 i5 i6 i7
+//
+// Where o0-o7 are the outgoing bits, and i0-i7 are the incoming bits.
 type Controller struct {
-	data    uint8
-	control uint8
+	data    uint8 // holds the data register, AKA types.SB.
+	control uint8 // holds the control register, AKA types.SC.
 
-	count uint8
+	count           uint8 // the number of bits that have been transferred.
+	internalClock   bool  // if true, this controller is the master.
+	transferRequest bool  // if true, a transfer has been requested.
 
-	irq               *interrupts.Service
-	attachedDevice    *Controller
-	current           uint8
-	resultFallingEdge bool
+	irq               *interrupts.Service // the interrupt service.
+	attachedDevice    *Controller         // the device that is attached to this controller.
+	resultFallingEdge bool                // the result of the last falling edge. (Bit 8 of DIV: 8.192 kHz)
 }
 
 func (c *Controller) Attach(d *Controller) {
@@ -31,9 +50,7 @@ func (c *Controller) Attach(d *Controller) {
 
 func NewController(irq *interrupts.Service) *Controller {
 	c := &Controller{
-		irq:     irq,
-		current: currentSerial,
-		control: 0x7E,
+		irq: irq,
 	}
 	types.RegisterHardware(types.SB, func(v uint8) {
 		c.data = v
@@ -42,30 +59,30 @@ func NewController(irq *interrupts.Service) *Controller {
 	})
 	types.RegisterHardware(types.SC, func(v uint8) {
 		c.control = v | 0x7E
+		c.internalClock = (v & types.Bit0) == types.Bit0
+		c.transferRequest = (v & types.Bit7) == types.Bit7
 	}, func() uint8 {
-		return c.control
+		return c.control | 0x7E
 	})
-	currentSerial++
 	return c
 }
 
 // Tick ticks the serial controller.
 func (c *Controller) Tick(div uint16) {
 	// is the serial transfer enabled?
-	if !c.transferRequest() || !c.internalClock() {
+	if !c.internalClock || !c.transferRequest {
 		return
 	}
 	if c.resultFallingEdge && !c.getFallingEdge(div) {
-		if c.count <= 8 {
-			bit := c.attachedDevice.send()
-			c.attachedDevice.receive(c.data&types.Bit7 == types.Bit7)
+		bit := c.attachedDevice.send()
+		c.attachedDevice.receive(c.data&types.Bit7 == types.Bit7)
 
-			c.data = c.data << 1
-			if bit {
-				c.data |= 1
-			}
-			c.checkTransfer()
+		c.data = c.data << 1
+		if bit {
+			c.data |= 1
 		}
+
+		c.checkTransfer()
 	}
 	c.resultFallingEdge = c.getFallingEdge(div)
 }
@@ -77,14 +94,14 @@ func (c *Controller) checkTransfer() {
 
 		// clear transfer request
 		c.control &^= types.Bit7
+		c.transferRequest = false
 	}
 }
 
+// send returns the
 func (c *Controller) send() bool {
-	if c == nil {
-		return false
-	}
-	if c.internalClock() {
+	// if c is nil, or this is the master, return true.
+	if c == nil || c.internalClock {
 		return true
 	}
 	return (c.data & types.Bit7) == types.Bit7
@@ -94,7 +111,7 @@ func (c *Controller) receive(bit bool) {
 	if c == nil {
 		return
 	}
-	if !c.internalClock() {
+	if !c.internalClock {
 		c.data = c.data << 1
 		if bit {
 			c.data |= 1
@@ -105,15 +122,5 @@ func (c *Controller) receive(bit bool) {
 
 // getFallingEdge returns true if the falling edge of the clock is reached.
 func (c *Controller) getFallingEdge(div uint16) bool {
-	return ((div & (1 << 8)) != 0) && c.internalClock() && c.transferRequest()
-}
-
-// transferRequest returns true if a transfer is requested.
-func (c *Controller) transferRequest() bool {
-	return c.control&types.Bit7 != 0
-}
-
-// internalClock returns true if the internal clock is used.
-func (c *Controller) internalClock() bool {
-	return c.control&types.Bit0 != 0
+	return ((div & (1 << 8)) != 0) && c.internalClock && c.transferRequest
 }
