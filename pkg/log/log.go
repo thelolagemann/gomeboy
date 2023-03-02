820668cec46c261f1afb6b5d159cf0041d2e3da8
Harry Bond <15960568+thelolagemann@users.noreply.github.com>
2023-03-02 04:28:37 +0000

    added basic logger

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,7 +1,28 @@
 package log
 
+import "fmt"
+
 type Logger interface {
 	Infof(format string, args ...interface{})
 	Errorf(format string, args ...interface{})
 	Debugf(format string, args ...interface{})
+}
+
+type logger struct {
+}
+
+func New() Logger {
+	return &logger{}
+}
+
+func (l *logger) Infof(format string, args ...interface{}) {
+	fmt.Printf("[INFO]\t"+format+"\n", args...)
+}
+
+func (l *logger) Errorf(format string, args ...interface{}) {
+	fmt.Printf("[ERROR]\t"+format+"\n", args...)
+}
+
+func (l *logger) Debugf(format string, args ...interface{}) {
+	fmt.Printf("[DEBUG]\t"+format+"\n", args...)
 }
