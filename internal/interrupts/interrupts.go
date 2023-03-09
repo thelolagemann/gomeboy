b3b56806f298554c4b146eb3fced2dd1fc131bd2
Harry Bond <15960568+thelolagemann@users.noreply.github.com>
2023-03-09 07:19:29 +0000

    improved interrupts documentation

--- internal/interrupts/interrupts.go
+++ internal/interrupts/interrupts.go
@@ -4,57 +4,53 @@ import (
 	"github.com/thelolagemann/go-gameboy/internal/types"
 )
 
-// Address is an address of an interrupt. When an interrupt occurs,
-// the CPU jumps to the specified interrupt address.
-type Address uint16
-
 const (
-	// VBlank is the VBL interrupt address.
-	VBlank Address = 0x40
-	// LCD is the LCD interrupt address.
-	LCD Address = 0x48
-	// Timer is the Timer interrupt address.
-	Timer Address = 0x50
-	// Serial is the Serial interrupt address.
-	Serial Address = 0x58
-	// Joypad is the Joypad interrupt address.
-	Joypad Address = 0x60
+	// VBlankFlag is the VBlank interrupt flag (bit 0),
+	// which is requested every time the PPU enters
+	// VBlank mode (lcd.VBlank).
+	VBlankFlag = types.Bit0
+	// LCDFlag is the LCD interrupt flag (bit 1), which
+	// is requested by the LCD STAT register (types.STAT),
+	// when certain conditions are met.
+	LCDFlag = types.Bit1
+	// TimerFlag is the Timer interrupt flag (bit 2),
+	// which is requested when the timer overflows,
+	// (types.TIMA > 0xFF).
+	TimerFlag = types.Bit2
+	// SerialFlag is the Serial interrupt flag (bit 3),
+	// which is requested when a serial transfer is
+	// completed.
+	SerialFlag = types.Bit3
+	// JoypadFlag is the Joypad interrupt Flag (bit 4),
+	// which is requested when any of types.P1 bits 0-3
+	// go from high to low, if the corresponding select
+	// bit (types.P1 bit 4 or 5) is set to 0.
+	JoypadFlag = types.Bit4
 )
 
-// Flag is an interrupt flag, which simply specifies what bit of the
-// interrupt registers is used to access the interrupt.
-type Flag = uint8
-
-const (
-	// VBlankFlag is the VBL interrupt flag (bit 0).
-	VBlankFlag Flag = types.Bit0
-	// LCDFlag is the LCD interrupt flag (bit 1).
-	LCDFlag Flag = types.Bit1
-	// TimerFlag is the Timer interrupt flag (bit 2).
-	TimerFlag Flag = types.Bit2
-	// SerialFlag is the Serial interrupt flag (bit 3).
-	SerialFlag Flag = types.Bit3
-	// JoypadFlag is the Joypad interrupt flag (bit 4).
-	JoypadFlag Flag = types.Bit4
-)
-
-// Service represents the current state of the interrupts.
+// Service is the interrupt service, used to request
+// interrupts and to get the current interrupt vector.
+//
+// When an interrupt is requested, the corresponding bit
+// in the Flag register is set. When an interrupt is
+// enabled, the corresponding bit in the Enable register
+// is set. When an interrupt is requested and enabled,
+// and the IME is set, the CPU will jump to the interrupt
+// vector, and the corresponding bit in the Flag register
+// will be cleared.
+//
+// The IME is set by the DI, EI and RETI instructions,
+// and it is used to disable and enable interrupts.
 type Service struct {
-	// Flag is the Interrupt FlagRegister. (0xFF0F)
-	Flag uint8
-	// Enable is the Interrupt EnableRegister. (0xFFFF)
-	Enable uint8
-
-	// IME is the Interrupt Master Enable flag.
-	IME bool
+	Flag   uint8 // interrupt Flag (types.IF)
+	Enable uint8 // interrupt Enable (types.IE)
+	IME    bool  // interrupt Master Enable flag (IME)
 }
 
 // NewService returns a new Service.
 func NewService() *Service {
 	s := &Service{
-		Flag:   0xE1,
-		Enable: 0,
-		IME:    false,
+		Flag: 0xE1, // initial value of types.IF
 	}
 	// setup registers
 	types.RegisterHardware(
@@ -77,23 +73,35 @@ func NewService() *Service {
 	return s
 }
 
-// Request requests an interrupt.
-func (s *Service) Request(flag Flag) {
-	s.Flag = s.Flag | flag
+// HasInterrupts returns true if there are any interrupts
+// that are requested and enabled.
+func (s *Service) HasInterrupts() bool {
+	return s.Enable&s.Flag != 0
+}
+
+// Request requests the specified interrupt, by setting
+// the corresponding bit in the Flag register.
+func (s *Service) Request(flag uint8) {
+	s.Flag |= flag
 }
 
 // Vector returns the currently serviced interrupt vector,
 // or 0 if no interrupt is being serviced. This function
-// will also clear the interrupt flag.
-func (s *Service) Vector() Address {
+// will also clear the corresponding bit in the Flag
+// register.
+func (s *Service) Vector() uint16 {
 	if s.Enable&s.Flag == 0 {
 		return 0
 	}
 	for i := uint8(0); i < 5; i++ {
-		if s.Flag&(1<<i) != 0 && s.Enable&(1<<i) != 0 {
+		// get the flag for the current interrupt
+		flag := uint8(1 << i)
+
+		// check if the interrupt is requested and enabled
+		if s.Flag&(flag) != 0 && s.Enable&(flag) != 0 {
 			// clear the interrupt flag and return the vector
-			s.Flag = s.Flag ^ 1<<i
-			return Address(0x0040 + i*8)
+			s.Flag = s.Flag ^ flag
+			return uint16(0x0040 + i*8)
 		}
 	}
 
@@ -102,12 +110,24 @@ func (s *Service) Vector() Address {
 
 var _ types.Stater = (*Service)(nil)
 
+// Load implements the types.Stater interface.
+//
+// The values are loaded in the following order:
+//   - IME (bool)
+//   - Flag (uint8)
+//   - Enable (uint8)
 func (s *Service) Load(st *types.State) {
 	s.IME = st.ReadBool()
 	s.Flag = st.Read8()
 	s.Enable = st.Read8()
 }
 
+// Save implements the types.Stater interface.
+//
+// The values are saved in the following order:
+//   - IME (bool)
+//   - Flag (uint8)
+//   - Enable (uint8)
 func (s *Service) Save(st *types.State) {
 	st.WriteBool(s.IME)
 	st.Write8(s.Flag)
