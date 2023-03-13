dd74d83cf0fa64ad998b3b13230569de17f981f5
Harry Bond <15960568+thelolagemann@users.noreply.github.com>
2023-03-13 17:28:46 +0000

    added documentation to boot

--- internal/boot/boot.go
+++ internal/boot/boot.go
@@ -1,3 +1,6 @@
+// Package boot provides a boot ROM implementation for the Game Boy. Whilst
+// this package is not strictly required for the emulator to function, it
+// can be used to emulate the boot process of the Game Boy.
 package boot
 
 import (
@@ -6,90 +9,49 @@ import (
 	"fmt"
 )
 
-// ROM is a boot rom for the Game Boy. When the Game Boy first
-// powers on, the boot rom is mapped to memory addresses 0x0000 -
-// 0x00FF. The boot rom performs a series of tasks, such as
-// initializing the hardware, setting the stack pointer, scrolling
-// the Nintendo logo, etc. Once the boot rom has completed its
-// tasks, it is unmapped from memory, and the cartridge is mapped
-// over the boot rom, thus starting the cartridge execution, and
-// preventing the boot rom from being executed again.
+// ROM represents a boot ROM for the Game Boy. When the Game Boy first
+// powers on, the boot ROM is mapped to memory addresses 0x0000 -
+// 0x00FF (or 0x0000 - 0x00FF & 0x0200 - 0x08FF for the CGB).
+//
+// The boot ROM performs a series of tasks, such as initializing the
+// hardware, setting the stack pointer, scrolling the Nintendo logo, etc.
+//
+// Once the boot ROM has completed its tasks, it is unmapped from memory
+// (by writing to the types.BDIS register), and the cartridge is mapped
+// over the boot ROM, thus starting the cartridge execution, and preventing
+// the boot ROM from being executed again.
 type ROM struct {
-	raw []byte
-
-	// the md5 checksum of the boot rom
-	checksum string
+	raw      []byte // the raw boot rom
+	checksum string // the MD5 checksum of the boot rom
 }
 
-// NewBootROM returns a new boot rom for the Game
-// Boy. The boot rom should be provided as a byte slice,
-// and will be compared against the provided md5 checksum,
-// to ensure the boot rom is valid. There are multiple
-// situations where a panic will occur:
-//
-//  1. The boot rom is not a valid length (256 or 2304 bytes)
-//  2. The boot rom does not match the provided MD5 checksum
-//  3. The provided MD5 checksum is not a valid MD5 checksum (32 characters)
-//
-// Several checksums for various boot roms are provided in the
-// boot package for convenience, and can be used to verify the
-// provided boot rom. There are two boot roms provided in the
-// boot package, one for the DMG/MGB/SGB, and one for the CGB.
-//
-//	 Example:
-//
-//		// loading the DMG boot rom
-//		boot.NewBootROM(boot.DMGBootROM[:], boot.DMGBootROMChecksum)
-//		// loading the CGB boot rom
-//		boot.NewBootROM(boot.CGBBootROM[:], boot.CGBBootROMChecksum)
-func NewBootROM(raw []byte, md5Checksum string) *ROM {
+// LoadBootROM loads a boot ROM into a new ROM struct and returns a
+// pointer to it. The function ensures that the input raw slice has a
+// valid length for either DMG/MGB/SGB (256 bytes) or CGB (2304 bytes).
+// If the length is invalid, the function will panic. The function also
+// calculates the MD5 checksum of the boot rom, and stores it in the
+// ROM struct.
+func LoadBootROM(b []byte) *ROM {
 	// ensure correct lengths
-	if len(raw) != 256 && len(raw) != 2304 { // 256 bytes for DMG/MGB/SGB, 2304 bytes for CGB
-		panic(fmt.Sprintf("boot: invalid boot rom length: %d", len(raw)))
-	}
-
-	// validate checksum
-	if len(md5Checksum) != 32 {
-		panic(fmt.Sprintf("boot: invalid md5 checksum: %s", md5Checksum))
-	}
-	bootChecksum := md5.Sum(raw)
-	if md5Checksum != hex.EncodeToString(bootChecksum[:]) {
-		panic(fmt.Sprintf("boot: invalid checksum expected %s got %s", md5Checksum, hex.EncodeToString(bootChecksum[:])))
-	}
-
-	return &ROM{
-		raw: raw,
-	}
-}
-
-func LoadBootROM(raw []byte) *ROM {
-	// ensure correct lengths
-	if len(raw) != 256 && len(raw) != 2304 { // 256 bytes for DMG/MGB/SGB, 2304 bytes for CGB
-		panic(fmt.Sprintf("boot: invalid boot rom length: %d", len(raw)))
+	if len(b) != 256 && len(b) != 2304 { // 256 bytes for DMG/MGB/SGB, 2304 bytes for CGB
+		panic(fmt.Sprintf("boot: invalid boot rom length: %d", len(b)))
 	}
 
 	// calculate checksum
-	bootChecksum := md5.Sum(raw)
+	bootChecksum := md5.Sum(b)
 
 	return &ROM{
-		raw:      raw,
+		raw:      b,
 		checksum: hex.EncodeToString(bootChecksum[:]),
 	}
 }
 
-// Read implements the mmu.IOBus interface. This method
-// simply returns the byte at the provided address.
+// Read returns the byte at the given address.
 func (b *ROM) Read(addr uint16) byte {
 	return b.raw[addr]
 }
 
-// Write implements the mmu.IOBus interface. The boot rom
-// is read only, and will panic if a write is attempted.
-func (b *ROM) Write(addr uint16, val byte) {
-	panic("boot: illegal write to boot")
-}
-
-// Checksum returns the md5 checksum of the boot rom.
+// Checksum returns the MD5 checksum of the boot rom.
 func (b *ROM) Checksum() string {
 	if b == nil {
 		return ""
@@ -103,52 +65,83 @@ func (b *ROM) Model() string {
 	if b == nil {
 		return "none"
 	}
-	for model, checksum := range knownBootROMChecksums {
-		if checksum == b.checksum {
-			return model
-		}
+	if model, ok := knownBootROMChecksums[b.checksum]; ok {
+		return model
 	}
 	return "unknown"
 }
 
 // knownBootROMChecksums is a map of known boot rom checksums,
-// with the key being the model, and the value being the checksum.
+// with the key being the checksum, and the value being the
+// model of the boot rom.
 var knownBootROMChecksums = map[string]string{
-	"DMG0": DMGEarlyBootROMChecksum,
-	"DMG":  DMBBootROMChecksum,
-	"MGB":  MGBBootROMChecksum,
-	"CGB0": CGBEarlyBootROMChecksum,
-	"CGB":  CGBBootROMChecksum,
+	DMG0:         "Game Boy (DMG-0)",
+	DMG:          "Game Boy (DMG-01)",
+	MGB:          "Game Boy Pocket",
+	SGB:          "Super Game Boy",
+	SGB2:         "Super Game Boy 2",
+	CGB0:         "Game Boy Color (CGB-0)",
+	CGB:          "Game Boy Color (CGB-A/B/C/D/E)",
+	CGB_AGB:      "Game Boy Advance (AGB-001)",
+	FORTUNE:      "Fortune/Bitman 3000B",
+	GAME_FIGHTER: "Game Fighter",
+	MAX_STATION:  "Max Station",
 }
 
-// known boot rom checksums
 const (
-	// DMGEarlyBootROMChecksum is the checksum of the DMG early boot rom,
-	// a variant that was found in very early DMG units, only ever sold
-	// in Japan.
-	DMGEarlyBootROMChecksum = "a8f84a0ac44da5d3f0ee19f9cea80a8c"
-	// DMBBootROMChecksum is the checksum of the DMG boot rom, which is
-	// the boot rom found in the original B&W Game Boy.
-	DMBBootROMChecksum = "32fbbd84168d3482956eb3c5051637f5"
-	// MGBBootROMChecksum is the checksum of the MGB boot rom, which differs
-	// only by a single byte from the DMG boot rom, used to identify the
-	// MGB.
-	MGBBootROMChecksum = "71a378e71ff30b2d8a1f02bf5c7896aa"
-	// CGBEarlyBootROMChecksum is the checksum of the CGB early boot rom,
-	// a variant that was found in very early CGB units, only ever sold
-	// in Japan.
-	CGBEarlyBootROMChecksum = "7c773f3c0b01cb73bca8e83227287b7f"
-	// CGBBootROMChecksum is the checksum of the CGB boot rom, which is
-	// the boot rom found in the original CGB.
-	CGBBootROMChecksum = "dbfce9db9deaa2567f6a84fde55f9680"
+	// DMG0 is the checksum of the DMG early boot ROM,
+	// a variant that was found in very early DMG units and
+	// only ever sold in Japan. It has a different behaviour
+	// than the DMG boot ROM, in that in the case of a boot
+	// failure, it will flash the screen, rather than hanging
+	// after the Nintendo logo.
+	DMG0 = "a8f84a0ac44da5d3f0ee19f9cea80a8c"
+	// DMG is the checksum of the DMG boot rom, which is
+	// the most common boot ROM found in the original DMG-01
+	// models.
+	DMG = "32fbbd84168d3482956eb3c5051637f5"
+	// MGB is the checksum of the MGB boot ROM, which differs
+	// only by a single byte from the DMG boot ROM, loading
+	// the value 0xFF into the A register, rather than 0x01.
+	// This can be used by games to detect that it is running
+	// on MGB hardware, rather than a DMG.
+	MGB = "71a378e71ff30b2d8a1f02bf5c7896aa"
+	// SGB is the checksum of the SGB boot ROM, which has
+	// significant differences in behaviour to the DMG boot ROM.
+	// Instead of showing a logo animation, it instead sends the
+	// ROM cartridge header to the SNES via the SGB, and the
+	// SNES then shows an animation before displaying the game.
+	SGB = "d574d4f9c12f305074798f54c091a8b4"
+	// SGB2 is the checksum of the SGB2 boot ROM, similar in
+	// differences as the MGB boot ROM is to the DMG boot ROM,
+	// differing only by a single byte, which loads the value
+	// 0xFF into the A register, rather than 0x01. This can be
+	// used by games to detect that it is running on SGB2
+	// hardware, rather than on the original SGB.
+	SGB2 = "e0430bca9925fb9882148fd2dc2418c1"
+	// CGB0 is the checksum of the CGB early boot ROM, a variant
+	// that was found in very early CGB units. It has a few
+	// differences in behaviour to the CGB boot ROM
+	//  - it does not initialize Wave RAM
+	//  - has two redundant writes to RAM
+	//  - uses less optimized code to load the logo
+	CGB0 = "7c773f3c0b01cb73bca8e83227287b7f"
+	// CGB is the checksum of the CGB boot rom, which is
+	// the boot rom found in the most common CGB models. It
+	// has a larger size than the DMG boot ROMs (2304 bytes),
+	// and has increased functionality to support the CGB
+	// hardware.
+	CGB = "dbfce9db9deaa2567f6a84fde55f9680"
+	// CGB_AGB is the checksum of the boot ROM found in the GBA's
+	// GBC compatibility mode.
+	CGB_AGB = "e6cefb5f7d352fab6681989763917c73"
+	// FORTUNE is the checksum of the boot ROM found in the
+	// Game Boy clone "Fortune/Bitman 3000B".
+	FORTUNE = "92ed4eca17d61fcd53f8a64c3ce84743"
+	// GAME_FIGHTER is the checksum of the boot ROM found in the
+	// Game Boy clone "Game Fighter".
+	GAME_FIGHTER = "6a7b8ee12a793f66a969c6a2b8926cc9"
+	// MAX_STATION is the checksum of the boot ROM found in the
+	// Game Boy clone "Maxstation".
+	MAX_STATION = "77a7021db824010a678791f6d062943d"
 )
-
-// DMGBootROM is a boot rom for the DMG. This boot rom
-// should be 256 bytes in length, and will be used to
-// initialize the DMG hardware.
-var DMGBootROM = [0x100]byte{}
-
-// CGBBootROM is a boot rom for the CGB. This boot rom
-// should be 2304 bytes in length, and will be used to
-// initialize the CGB hardware.
-var CGBBootROM = [0x900]byte{}
