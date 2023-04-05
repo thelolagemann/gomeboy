f79c81f02ed337ff7d679dd0fcf3616217a57238
Harry Bond <15960568+thelolagemann@users.noreply.github.com>
2023-04-05 01:12:16 +0100

    added dma to scheduler

--- internal/ppu/dma.go
+++ internal/ppu/dma.go
@@ -2,116 +2,139 @@ package ppu
 
 import (
 	"github.com/thelolagemann/go-gameboy/internal/mmu"
+	"github.com/thelolagemann/go-gameboy/internal/scheduler"
 	"github.com/thelolagemann/go-gameboy/internal/types"
 )
 
+// DMA (Direct Memory Access) is used to transfer data from
+// the CPU's memory to the PPU's OAM (Object Attribute Memory).
+// It is used as a fast alternative to the CPU writing to the
+// OAM directly.
+//
+// The DMA transfer is done in 160 cycles, and the CPU is
+// halted during this time. The CPU is also halted for 4 cycles
+// after the DMA transfer is done.
 type DMA struct {
-	Enabled    bool
-	restarting bool
+	source      uint16
+	destination uint8
+	value       uint8
+	remaining   uint8 // 40 * 4 = 160
 
-	timer  uint16
-	source uint16
-	value  uint8
+	active     bool
+	enabled    bool
+	restarting bool
 
 	bus *mmu.MMU
 	oam *OAM
 
-	cycleFunc func()
+	s *scheduler.Scheduler
 }
 
-func (d *DMA) init() {
+func NewDMA(bus *mmu.MMU, oam *OAM, s *scheduler.Scheduler) *DMA {
+	d := &DMA{
+		bus: bus,
+		oam: oam,
+		s:   s,
+	}
+	s.RegisterEvent(scheduler.DMATransfer, d.doTransfer)
+	s.RegisterEvent(scheduler.DMAStartTransfer, d.startTransfer)
+	s.RegisterEvent(scheduler.DMAEndTransfer, func() {
+		d.active = false
+		d.enabled = false
+	})
+
 	// setup register
 	types.RegisterHardware(
 		types.DMA,
 		func(v uint8) {
-			d.value = v
-			d.source = uint16(v) << 8
-			d.timer = 0
+			source := uint16(v) << 8
 
-			d.restarting = d.Enabled
-			d.Enabled = true
-			d.cycleFunc()
+			// set the value (reading DMA simply returns the last value written)
+			// https://github.com/Gekkio/mooneye-test-suite/blob/main/acceptance/oam_dma/reg_read.s
+			d.value = v
+
+			// mark DMA as inactive (it will be active in 1-M-cycle)
+			d.active = false
+
+			// handle restarting DMA
+			d.restarting = d.enabled
+
+			// update new source, destination and remaining bytes
+			d.source = source
+			d.destination = 0
+			d.remaining = 160
+
+			// reschedule any existing DMA
+			if d.restarting {
+				// d.s.DescheduleEvent(scheduler.DMATransfer)
+				d.s.DescheduleEvent(scheduler.DMAStartTransfer)
+				d.s.DescheduleEvent(scheduler.DMATransfer)
+				d.s.DescheduleEvent(scheduler.DMAEndTransfer)
+			}
+
+			// mark DMA as enabled
+			d.enabled = true
+
+			// schedule DMA start for 1-M-cycle
+			d.s.ScheduleEvent(scheduler.DMAStartTransfer, 8)
+			//d.s.ScheduleEvent(scheduler.DMATransfer, 8)
+
 		},
 		func() uint8 {
 			return d.value
 		},
 	)
-}
 
-func NewDMA(bus *mmu.MMU, oam *OAM) *DMA {
-	d := &DMA{
-		bus: bus,
-		oam: oam,
-	}
-	d.init()
 	return d
 }
 
-// TickM ticks the DMA by 1 M-cycle (4 T-cycles)
-func (d *DMA) TickM() {
-	d.TickT()
-	d.TickT()
-	d.TickT()
-	d.TickT()
+func (d *DMA) startTransfer() {
+	d.active = true
+	d.doTransfer()
+	d.s.ScheduleEvent(scheduler.DMAEndTransfer, 640)
 }
 
-// TickT ticks the DMA by 1 T-cycle.
-func (d *DMA) TickT() {
-	// increment the timer
-	d.timer++
-
-	// every 4 ticks, transfer a byte to OAM
-	// takes 4 ticks to turn on, 640 ticks to transfer
-	if d.timer > 4 {
+func (d *DMA) doTransfer() {
+	if d.restarting {
 		d.restarting = false
-		if d.timer < 644 {
-			offset := (d.timer - 4) >> 2
-			currentSource := d.source + (offset)
-
-			// is a DMA trying to read from the OAM?
-			if currentSource >= 0xFE00 {
-				// if so, make sure we don't read from the OAM
-				// and instead read from the source address - 0x2000
-				currentSource -= 0x2000
-			}
-
-			// load the value from the source address
-			d.oam.Write(offset, d.bus.Read(currentSource))
-
-		}
 
 	}
-	if d.timer > 644 {
-		d.Enabled = false
-		d.timer = 0
-		d.cycleFunc()
+	// where are we transferring from?
+	currentSource := d.source
+	if currentSource >= 0xFE00 {
+		currentSource -= 0x2000
+	}
+
+	// transfer a byte from the source to the destination
+	d.oam.Write(uint16(d.destination), d.bus.Read(currentSource))
+
+	// increment source and destination
+	d.source++
+	d.destination++
+
+	// decrement remaining
+	d.remaining--
+
+	// if there are still bytes to transfer, schedule another transfer
+	if d.remaining > 0 {
+		d.s.ScheduleEvent(scheduler.DMATransfer, 4)
 	}
 }
 
 func (d *DMA) IsTransferring() bool {
-	return d.timer > 4 || d.restarting
+	return d.active || d.restarting
 }
 
 var _ types.Stater = (*DMA)(nil)
 
 func (d *DMA) Load(s *types.State) {
-	d.Enabled = s.ReadBool()
-	d.restarting = s.ReadBool()
-	d.timer = s.Read16()
 	d.source = s.Read16()
 	d.value = s.Read8()
 	d.oam.Load(s)
 }
 
 func (d *DMA) Save(s *types.State) {
-	s.WriteBool(d.Enabled)
-	s.WriteBool(d.restarting)
-	s.Write16(d.timer)
 	s.Write16(d.source)
 	s.Write8(d.value)
 	d.oam.Save(s)
-}
-
-func (d *DMA) AttachRegenerate(cycle func()) {
-	d.cycleFunc = cycle
 }
