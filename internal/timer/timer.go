b2be3c02cfb7645e877cffcd7dd0d76ba065eee9
Harry Bond <15960568+thelolagemann@users.noreply.github.com>
2023-04-08 02:51:09 +0100

    improved timer documentation

--- internal/timer/timer.go
+++ internal/timer/timer.go
@@ -12,9 +12,9 @@ import (
 type Controller struct {
 	currentBit uint8 // the current bit of the DIV register that is used to increment the TIMA register
 
-	tima uint8 // timer counter
-	tma  uint8 // timer modulo
-	tac  uint8 // timer control
+	tima uint8 // types.TIMA
+	tma  uint8 // types.TMA
+	tac  uint8 // types.TAC
 
 	irq *interrupts.Service
 	s   *scheduler.Scheduler
@@ -34,6 +34,7 @@ func NewController(irq *interrupts.Service, s *scheduler.Scheduler) *Controller 
 		lastCycle: 0x5433,
 	}
 
+	// set up events
 	s.RegisterEvent(scheduler.TimerInterrupt, func() {
 		c.irq.Request(interrupts.TimerFlag)
 	})
@@ -42,6 +43,8 @@ func NewController(irq *interrupts.Service, s *scheduler.Scheduler) *Controller 
 	s.RegisterEvent(scheduler.TimerTIMAFinishReload, func() {
 		c.reloading = false
 	})
+
+	// set up registers
 	types.RegisterHardware(
 		types.DIV,
 		func(v uint8) {
@@ -76,7 +79,6 @@ func NewController(irq *interrupts.Service, s *scheduler.Scheduler) *Controller 
 
 		}))
 
-	// set up registers
 	types.RegisterHardware(
 		types.TIMA,
 		func(v uint8) {
@@ -113,27 +115,24 @@ func NewController(irq *interrupts.Service, s *scheduler.Scheduler) *Controller 
 		func(v uint8) {
 			oldBit := c.currentBit
 			c.changeSpeed(v & 0b11)
+
+			// disabling the timer could cause an abrupt increment
+			// if the selected bit of DIV is 1, as disabling the timer
+			// will disconnect the DIV register from the timer, thus
+			// causing a falling edge to be detected
 			if c.enabled && v&types.Bit2 == 0 {
 				internal := uint16((s.Cycle() - c.lastCycle) & 0xFFFF)
 				if internal&timerBits[oldBit] != 0 {
-					c.tima++
-
-					// if the timer overflows, reload it
-					if c.tima == 0 {
-						c.tima = c.tma
-						c.irq.Request(interrupts.TimerFlag)
-					}
+					c.abruptlyIncrementTIMA()
 				}
 			}
-			c.enabled = v&types.Bit2 == types.Bit2
 
-			// if the timer was disabled, a sp
+			// update enabled flag
+			c.enabled = v&types.Bit2 == types.Bit2
 		}, func() uint8 {
 			return c.tac | 0b11111000
 		},
 	)
-
-	// c.s.ScheduleEvent(scheduler.TimerTIMAIncrement, uint64(timaCycles[c.currentBit]*2))
 
 	return c
 }
@@ -163,7 +162,22 @@ func (c *Controller) reloadTIMA() {
 	c.s.ScheduleEvent(scheduler.TimerTIMAFinishReload, 1)
 }
 
-func (c *Controller) timaIncrement(delay bool) {
+// abruptlyIncrementTIMA is called when conditions are met
+// that would cause an abrupt increment of the timer.
+func (c *Controller) abruptlyIncrementTIMA() {
+	c.tima++
+
+	// an abrupt increment that causes a reload is performed
+	// instantly, rather than being delayed by 1-M cycle
+	if c.tima == 0 {
+		c.tima = c.tma
+		c.irq.Request(interrupts.TimerFlag)
+	}
+}
+
+// scheduledTIMAIncrement is called by the scheduler when the timer
+// should increment.
+func (c *Controller) scheduledTIMAIncrement() {
 	if c.enabled {
 		c.tima++
 
@@ -174,47 +188,21 @@ func (c *Controller) timaIncrement(delay bool) {
 			c.reloadPending = true
 
 			// schedule TIMA reload
-			if delay {
-				c.s.ScheduleEvent(scheduler.TimerTIMAReload, 4)
-			} else {
-				c.s.DescheduleEvent(scheduler.TimerTIMAIncrement)
-				c.s.DescheduleEvent(scheduler.TimerTIMAReload)
-				c.s.DescheduleEvent(scheduler.TimerTIMAFinishReload)
-				c.s.DescheduleEvent(scheduler.TimerInterrupt)
-				c.tima = c.tma
-				c.s.ScheduleEvent(scheduler.TimerTIMAReload, 0)
-				//c.irq.Request(interrupts.TimerFlag)
-				c.s.ScheduleEvent(scheduler.TimerInterrupt, 0)
-			}
+			c.s.ScheduleEvent(scheduler.TimerTIMAReload, 4)
 		}
 	}
-}
-
-// abruptlyIncrementTIMA is called when conditions are met
-// that would cause an abrupt increment of the timer.
-func (c *Controller) abruptlyIncrementTIMA() {
-	c.tima++
-
-	// an abrupt increment that causes a reload is performed
-	// instantly, rather than in 1-M-cycle
-	if c.tima == 0 {
-		c.tima = c.tma
-		c.irq.Request(interrupts.TimerFlag)
-	}
-}
-
-// scheduledTIMAIncrement is called by the scheduler when the timer
-// should increment.
-func (c *Controller) scheduledTIMAIncrement() {
-	c.timaIncrement(true)
 	c.s.ScheduleEvent(scheduler.TimerTIMAIncrement, timaCycles[c.currentBit])
 }
 
+// changeSpeed changes the speed of the timer, rescheduling
+// any events that are affected by the change.
 func (c *Controller) changeSpeed(newBit uint8) {
 	internal := uint16((c.s.Cycle() - c.lastCycle) & 0xFFFF)
 
+	// changing the speed could cause an abrupt increment if the
+	// currently selected bit of DIV is 1, and the new bit is 0
 	if internal&timerBits[c.currentBit] != 0 && internal&timerBits[newBit] == 0 {
-		c.timaIncrement(false)
+		c.abruptlyIncrementTIMA()
 	}
 
 	ticksUntilIncrement := (rescheduleMasks[newBit] + 1) - (internal & rescheduleMasks[newBit])
@@ -233,6 +221,40 @@ var rescheduleMasks = [4]uint16{
 	0b111111,
 	0b11111111,
 }
+
+// timerBits is a lookup table for the bits of the DIV register
+// that are used by each timer speed. For example, if bit 9 is
+// set, then the timer will increment every 1024 cycles, as that is when
+// bit 9 of the DIV register would cause a falling edge. Here are some
+// examples to help visualize this:
+//
+// Bit 9: (1024 cycles)
+//
+//	Cycle 1023 (0b11_1111_1111) -> 1024 (0b100_0000_0000)
+//	               ^ ----------------------- ^ = falling edge
+//	Cycle 2047 (0b111_1111_1111) -> 2048 (0b1000_0000_0000)
+//	                ^ ------------------------ ^ = falling edge
+//
+// Bit 3: (16 cycles)
+//
+//	Cycle 15 (0b1111) -> 16 (0b1_0000)
+//	           ^ ------------- ^ = falling edge
+//	Cycle 31 (0b1_1111) -> 32 (0b10_0000)
+//	              ^ --------------- ^ = falling edge
+//
+// Bit 5: (64 cycles)
+//
+//	Cycle 63 (0b11_1111) -> 64 (0b100_0000)
+//	             ^ ---------------- ^ = falling edge
+//	Cycle 127 (0b111_1111) -> 128 (0b1000_0000)
+//	               ^ ------------------ ^ = falling edge
+//
+// Bit 7: (256 cycles)
+//
+//	Cycle 255 (0b1111_1111) -> 256 (0b1_0000_0000)
+//	              ^ -------------------- ^ = falling edge
+//	Cycle 511 (0b1_1111_1111) -> 512 (0b10_0000_0000)
+//	                ^ --------------------- ^ = falling edge
 var timerBits = [4]uint16{
 	// bit 9
 	0b1000000000,
