a358d0d60c99285dd80f706731a29b84e7e33b57
Harry Bond <15960568+thelolagemann@users.noreply.github.com>
2023-06-08 01:15:38 +0100

    added null logger

---
+++ pkg/log/null.go
@@ -0,0 +1,18 @@
+package log
+
+// nullLogger is a logger that does nothing.
+type nullLogger struct{}
+
+func (n nullLogger) Infof(format string, args ...interface{}) {
+}
+
+func (n nullLogger) Errorf(format string, args ...interface{}) {
+}
+
+func (n nullLogger) Debugf(format string, args ...interface{}) {
+}
+
+// NewNullLogger returns a logger that does nothing.
+func NewNullLogger() Logger {
+	return &nullLogger{}
+}
