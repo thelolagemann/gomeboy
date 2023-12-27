thelolagemann/gomeboy

commit 1e2b9c632204fd4b1dd07d668492db9a96f3cc50

small tweak to OAM catchup mechanics (#38)

internal/ppu/ppu.go
@@ -83,6 +83,7 @@ type PPU struct {
 	modeToInterrupt       uint8
 	currentLine           uint8
 	backgroundLineChanged [256]bool
+	lyCompare             byte
 }
 
 func New(b *io.Bus, s *scheduler.Scheduler) *PPU {
@@ -213,7 +214,7 @@ func New(b *io.Bus, s *scheduler.Scheduler) *PPU {
 		p.b.Set(types.LY, v.(uint8))
 	})
 	b.ReserveAddress(types.LYC, func(v byte) byte {
-		p.b.Set(types.LYC, v) // so STAT update is aware
+		p.lyCompare = v
 		// do we need to force a re-render on background
 		if b.Get(types.LYC) != v {
 			p.dirtyBackground(lyc)
@@ -811,7 +812,7 @@ func (p *PPU) statUpdate() {
 	prevInterruptLine := p.statInterruptLine
 
 	// handle LY=LYC
-	if p.lyForComparison == p.b.Get(types.LYC) {
+	if p.lyForComparison == p.lyCompare {
 		p.lycInterruptLine = true
 		p.status |= types.Bit2
 	} else {
@@ -853,10 +854,9 @@ func (p *PPU) renderScanline() {
 	if p.b.Get(types.LY) >= ScreenHeight {
 		return
 	}
-	if (!p.backgroundLineRendered[p.b.Get(types.LY)] || p.oam.spriteScanlines[p.b.Get(types.LY)] || p.oam.dirtyScanlines[p.b.Get(types.LY)] || p.backgroundDirty) && (p.BackgroundEnabled || p.b.IsGBCCart()) {
+	if (!p.backgroundLineRendered[p.b.Get(types.LY)] || p.oam.dirtyScanlines[p.b.Get(types.LY)] || p.backgroundDirty) && (p.BackgroundEnabled || p.b.IsGBCCart()) {
 		p.renderBackgroundScanline()
 
-		//fmt.Printf("%v rendered: %v sprite (dirty): %v (%v) background (dirty): %v (%v)\n", p.b.Get(types.LY), p.backgroundLineRendered[p.b.Get(types.LY)], p.oam.spriteScanlines[p.b.Get(types.LY)], p.oam.dirtyScanlines[p.b.Get(types.LY)], p.BackgroundEnabled, p.backgroundDirty)
 	}
 
 	if !p.Debug.WindowDisabled {
@@ -1080,13 +1080,7 @@ func (p *PPU) calculateTileID(tilemapOffset, lineOffset uint8, mapOffset uint8) 
 
 func (p *PPU) renderSpritesScanline(scanline uint8) {
 	if p.b.OAMChanged() {
-		// copy new data to oam
-		data := make([]byte, 160)
-		p.b.CopyFrom(0xFE00, 0xFEA0, data)
-		for i := 0; i < 160; i++ {
-			p.oam.Write(uint16(i), data[i])
-		}
-		p.b.OAMCatchup()
+		p.b.OAMCatchup(p.oam.Write)
 	}
 
 	spriteXPerScreen := [ScreenWidth]uint8{}
@@ -1180,12 +1174,7 @@ func (p *PPU) renderSpritesScanline(scanline uint8) {
 				}
 			}
 
-			// has the sprite changed the background?
-			if p.PreparedFrame[scanline][pixelPos] != pal[color] {
-				p.backgroundLineRendered[scanline] = false
-				// draw the pixel
-				p.PreparedFrame[scanline][pixelPos] = pal[color]
-			}
+			p.PreparedFrame[scanline][pixelPos] = pal[color]
 
 			// mark the pixel as occupied
 			spriteXPerScreen[pixelPos] = spriteX + 10

internal/apu/apu.go
@@ -8,8 +8,8 @@ import (
 )
 
 const (
-	bufferSize           = 256
-	emulatedSampleRate   = 65536
+	bufferSize           = 512
+	emulatedSampleRate   = 44100
 	samplePeriod         = 4194304 / emulatedSampleRate
 	frameSequencerRate   = 512
 	frameSequencerPeriod = 4194304 / frameSequencerRate
@@ -341,6 +341,8 @@ func (a *APU) sample() {
 		}
 		a.bufferPos = 0
 	}
+
+	a.s.ScheduleEvent(scheduler.APUSample, samplePeriod)
 }
 
 // Pause pauses the APU.

internal/io/dma.go
@@ -84,7 +84,10 @@ func (b *Bus) OAMChanged() bool {
 // OAMCatchup sets the OAMChanged flag to false. This is used
 // by the PPU to indicate to the bus that it has caught up to
 // the current values in the OAM.
-func (b *Bus) OAMCatchup() {
+func (b *Bus) OAMCatchup(f func(uint16, byte)) {
+	for i := uint16(0); i < 160; i++ {
+		f(i, b.data[0xfe00+i])
+	}
 	b.oamChanged = false
 }
 

internal/scheduler/scheduler.go
@@ -79,26 +79,21 @@ func (s *Scheduler) RegisterEvent(eventType EventType, fn func()) {
 // should be executed.
 func (s *Scheduler) Tick(c uint64) {
 	// what cycle are we advancing to
-	s.cycles += c
-
-	// handle sampling
-	if s.cycles%64 == 0 {
-		s.sampler()
-	}
-
-	// get the current cycle at which the next event occurs
-	nextEvent := s.nextEventAt
+	cycleToAdvance := s.cycles + c
 
 	// if the next event occurs in the future, simply increment
 	// the cycle count and return
-	if nextEvent > s.cycles {
+	if s.nextEventAt > cycleToAdvance {
+		s.cycles = cycleToAdvance
 		return
 	}
+
+	// get the cycle at which the next event occurs
+	nextEvent := s.nextEventAt
 
 	// otherwise perform accurate loop, jumping from each event
 	// (mostly to pass blarggs wave read/write/trigger - maybe make
 	// this optional in the future?)
-	cycleToAdvance := s.cycles
 	for nextEvent <= cycleToAdvance {
 		s.cycles = nextEvent
 

pkg/audio/sdl.go
@@ -104,7 +104,7 @@ var (
 
 const (
 	bufferSize = 512
-	sampleRate = 65536
+	sampleRate = 44100
 )
 
 func PlaySDL(data []byte) {

commit 13eae4d2d562eb49525c9d7930c8325008587832

fixed sprite cache regression introduced in 1e2b9c6

internal/ppu/ppu.go
@@ -824,7 +824,8 @@ func (p *PPU) statUpdate() {
 }
 
 func (p *PPU) renderScanline() {
-	if (!p.backgroundLineRendered[p.b.Get(types.LY)] || p.dirtyScanlines[p.b.Get(types.LY)] || p.backgroundDirty) && (p.bgEnabled || p.b.IsGBCCart()) {
+	currentScanline := p.b.Get(types.LY)
+	if (!p.backgroundLineRendered[currentScanline] || p.dirtyScanlines[currentScanline] || p.backgroundDirty) && (p.bgEnabled || p.b.IsGBCCart()) {
 		p.renderBackgroundScanline()
 	}
 
@@ -833,7 +834,7 @@ func (p *PPU) renderScanline() {
 	}
 
 	if p.objEnabled && !p.Debug.SpritesDisabled {
-		p.renderSpritesScanline(p.b.Get(types.LY))
+		p.renderSpritesScanline(currentScanline)
 	}
 }
 
@@ -985,7 +986,11 @@ func (p *PPU) renderSpritesScanline(scanline uint8) {
 				continue
 			}
 
-			p.PreparedFrame[scanline][pixelPos] = pal[colourNumber]
+			// did rendering this sprite change the background
+			if p.PreparedFrame[scanline][pixelPos] != pal[colourNumber] {
+				p.backgroundLineRendered[scanline] = false
+				p.PreparedFrame[scanline][pixelPos] = pal[colourNumber]
+			}
 
 			// mark the pixel as occupied
 			spriteXPerScreen[pixelPos] = sprite.x + 8